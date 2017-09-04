89e13d7cfba394cd648258558755adda3c5abaf7
Tsukasa Muto <tsukasa610@gmail.com>
2017-09-04 22:53:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,22 +1,12 @@
 
 
 
-=begin def turn_count(board)
-  count = 0
+def turn_count(board)
+  turns = 0
   board.each do |position|
     if position == "X" || position = "O"
-      count += 1
+      turns += 1
     end
   end
-  count
-end
-=end
-def turn_count(board)
- turns = 0
- board.each do |token|
-   if token == "X" || token == "O"
-     turns+=1
-   end
- end
- turns
+  turns
 end
